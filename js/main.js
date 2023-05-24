911e4513f9d8b6e7ab0e9c8c95f86d2c77ad5958
sy-0825 <71171668+sy-0825@users.noreply.github.com>
2023-05-25 02:21:53 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// ハンバーガーメニュー
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+  const link = document.querySelector('.overlay-link');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  link.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+};
